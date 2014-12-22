9e99d6dde2c2251dc85eebf0d5b2af46ea603673
Chris Rempola <chrisxstyles@gmail.com>
2014-12-21 23:32:16 -0800

    Adding more events

--- js/app.js
+++ js/app.js
@@ -8,5 +8,18 @@ $(document).ready(function() {
 		//alert('mouse left');
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
+	})
+	.mousedown(function(){
+		//console.log('mousedown');
+		//plan hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// animate the hadouken to the right of the screen
+	})
+	.mouseup(function(){
+		//console.log('mouseup');
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
 })
