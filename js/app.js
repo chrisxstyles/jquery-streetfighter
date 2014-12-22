80ea7650386ac742a5d9009e08ced41a27e03a55
Chris Rempola <chrisxstyles@gmail.com>
2014-12-21 23:55:43 -0800

    Animating the hadouken and adding sound

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	function playHadouken(){
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
 	$('.ryu').mouseenter(function(){
 		//alert('mouse entered .ryu div')
 		$('.ryu-still').hide();
@@ -11,11 +16,18 @@ $(document).ready(function() {
 	})
 	.mousedown(function(){
 		//console.log('mousedown');
-		//plan hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate the hadouken to the right of the screen
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
 	})
 	.mouseup(function(){
 		//console.log('mouseup');
